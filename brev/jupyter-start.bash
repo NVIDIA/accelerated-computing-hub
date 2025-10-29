8d6fa71e847bc99f575564f341e9c9e9c6ae2c8d
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2025-10-29 13:58:01 -0400

    Jupyter: Rename `jupyter-generate-settings.bash` to `jupyter-generate-plugin-settings.bash`.

--- brev/jupyter-start.bash
+++ brev/jupyter-start.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/accelerated-computing-hub/brev/jupyter-generate-settings.bash
+/accelerated-computing-hub/brev/jupyter-generate-plugin-settings.bash
 
 mkdir -p /accelerated-computing-hub/logs
 
