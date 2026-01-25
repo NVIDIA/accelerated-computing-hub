126bf42cf7209a38a4afeaa343ad9a1f2c5e8f3a
Cole Brower <cbrower@nvidia.com>
2026-01-25 13:35:39 -0800

    add: notebook sources and support files

---
+++ tutorials/floating-point-emulation/cpp_source/include/cuda_utilities.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef CUDA_CHECK_AND_EXIT
+#    define CUDA_CHECK_AND_EXIT(error)                                                                      \
+        {                                                                                                   \
+            auto status = static_cast<cudaError_t>(error);                                                  \
+            if (status != cudaSuccess) {                                                                    \
+                std::cout << cudaGetErrorString(status) << " " << __FILE__ << ":" << __LINE__ << std::endl; \
+                std::exit(status);                                                                          \
+            }                                                                                               \
+        }
+#endif
