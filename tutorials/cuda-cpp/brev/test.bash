e4862f0aacb7087141d8746880ff371f89df6ce2
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2026-02-23 11:22:52 -0500

    Tutorials/CUDA C++: Rebase on to CUDA 13.1 container.

--- tutorials/cuda-cpp/brev/test.bash
+++ tutorials/cuda-cpp/brev/test.bash
@@ -1,3 +1,18 @@
 #! /bin/bash
 
+START_TIME=$(date +%s.%N)
+
 nvidia-smi
+
+END_TIME=$(date +%s.%N)
+ELAPSED=$(awk "BEGIN {print $END_TIME - $START_TIME}")
+
+echo ""
+awk -v elapsed="$ELAPSED" 'BEGIN {
+    hours = int(elapsed / 3600)
+    minutes = int((elapsed % 3600) / 60)
+    seconds = elapsed % 60
+    printf "Elapsed time: %dh %dm %.3fs\n", hours, minutes, seconds
+}'
+
+exit 0
