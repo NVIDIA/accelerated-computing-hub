3e80252d7c08b688972500a013a5432e4eb6d358
Juan Galvez <jjgalvez@msn.com>
2026-07-10 09:38:49 -0500

    Full nvmath-python MGMN tutorial

--- tutorials/nvmath-python/notebooks/_mgmn_nb_setup.py
+++ tutorials/nvmath-python/notebooks/_mgmn_nb_setup.py
@@ -1,18 +1,31 @@
 from typing import Union, Literal  # to suppress irrelevant warnings from nbdistributed
 
-from IPython.core.magic import register_cell_magic, needs_local_scope
+from IPython.core.magic import register_cell_magic
 from IPython import get_ipython
 
 @register_cell_magic
-@needs_local_scope
-def mgmn(line, cell, local_ns=None):
+def mgmn(line, cell):
     ipython = get_ipython()
-    if local_ns and local_ns["using_mpi"]:
+    magics = ipython.magics_manager.magics["cell"]
+
+    mpi_workers = False
+    torch_distributed_workers = False
+
+    if "px" in magics:
+        view = magics["px"].__self__.view
+        if view and len(view.client.ids) != 0:
+            mpi_workers = True
+
+    if "distributed" in magics and magics["distributed"].__self__._comm_manager is not None:
+        torch_distributed_workers = True
+
+    if mpi_workers and torch_distributed_workers:
+        import logging
+        logging.warning("Both the MPI and torch.distributed workers are running. Executing only on MPI workers.")
+
+    if mpi_workers:
         ipython.run_cell_magic("px", "", cell)
-    elif local_ns and local_ns["using_torch_distributed"]:
+    elif torch_distributed_workers:
         ipython.run_cell_magic("distributed", "", cell)
     else:
-        print("No worker processes running")
-
-using_torch_distributed = False
-using_mpi = False
+        print("No worker processes running.")
